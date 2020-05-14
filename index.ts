a0cd0dce86aa60ae94cb49c35d957e08868001ba
jaybo <wangjiebo911@gmail.com>
2020-05-14 13:50:35 +0800

    style: format

--- index.ts
+++ index.ts
@@ -1,27 +1,41 @@
-"use strict";
-const {
-  NativeModules,
-  NativeEventEmitter,
-  DeviceEventEmitter,
-  Platform,
-} = require("react-native");
+import { NativeModules, Platform } from 'react-native';
+
 const { UGBleModule } = NativeModules;
 /**
  * 扫描设备的通知
  */
 export const scanBluetoothDeviceNotification =
-  "scanBluetoothDeviceNotification";
+  'scanBluetoothDeviceNotification';
 /**
  * 连接设备状态的通知
  */
-export const connectDeviceTypeNotification = "connectDeviceTypeNotification";
+export const connectDeviceTypeNotification = 'connectDeviceTypeNotification';
 
 export enum ConnectStatusType {
-  CONNECT_DEVICE_TYPE_SUCCESS = 0, //连接成功
-  CONNECT_DEVICE_TYPE_ONFAIL = 1, //连接失败
-  CONNECT_DEVICE_TYPE_DISCONNECT = 3, //断开连接
-  CONNECT_DEVICE_TYPE_ADDRESS_NULL = 4, //设备地址为空
-  CONNECT_DEVICE_TYPE_BLUETOOTH_CLOSE = 5, //蓝牙未打开
+  /**
+   * 连接成功
+   */
+  CONNECT_DEVICE_TYPE_SUCCESS = 0,
+
+  /**
+   * 连接失败
+   */
+  CONNECT_DEVICE_TYPE_ONFAIL = 1,
+
+  /**
+   * 断开连接
+   */
+  CONNECT_DEVICE_TYPE_DISCONNECT = 3,
+
+  /**
+   * 设备地址为空
+   */
+  CONNECT_DEVICE_TYPE_ADDRESS_NULL = 4,
+
+  /**
+   * 蓝牙未打开
+   */
+  CONNECT_DEVICE_TYPE_BLUETOOTH_CLOSE = 5,
 }
 /**
  * 蓝牙是否打开
@@ -52,10 +66,10 @@ export const startScanAndTime = (scanTime: number) => {
 };
 
 export const stopScan = async () => {
-  if (Platform.OS ==='ios') {
-    return UGBleModule.stopScan()
+  if (Platform.OS === 'ios') {
+    return UGBleModule.stopScan();
   }
-}
+};
 
 /**
  * 连接设备并接收数据
